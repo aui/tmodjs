fe0c3615b71e6e3b15fb443a90f31585246327ba
luu <jiawu.lu@gmail.com>
2013-05-25 14:54:32 +0800

    update it

--- README.md
+++ README.md
@@ -2,156 +2,23 @@
 
 <http://cdc-im.github.io/atc>
 
+## 修改点
 
-##	前言
+node compiler.js --namespace taobao  demo/templates/
 
-随着 HTML5 时代到来，前端技术在不断的发展壮大，我们为此欣喜若狂。但作为一名前端开发者，我们也不无时无刻的感受到很多看似平常的技术因为浏览器制约难以在前端中实现，其中之一便是模板技术。
-	
-虽然我们已经有了很多优秀的前端模板引擎可以选择，但前端模板技术与服务端模板技术却完全不是一个层面的东西，受浏览器限制，前端模板按文件与目录组织、include 这些基本的特性几乎无法实现，如：
-
-	<%include('./public/header')%>
-	<h3><%=title%></h3>
-	<ul>
-	    <% for (i = 0; i < list.length; i ++) { %>
-	       <li>
-	        	<a href="<%= list[i].url %>">
-	        		<%= list[i].title %>
-	        	</a>
-	       </li>
-	    <% } %>
-	</ul>
-	<%include('./public/footer')%>
-	
-而前端模板最佳实践是把所有模板使用``<script type="text/html" ..``标签写在页面里面。[示例](http://aui.github.io/artTemplate/demo/basic.html) 
-
-随着应用越来越复杂、模板越来越多、模板各种依赖出现的时候，我们的页面也变得臃肿不堪、难以维护，以至于我们不得不拿出各种技巧进行所谓优化，如抽离到js中、按需加载等。回过头再来看这些最佳实践，我们不经意之间为了解决一个问题引发了更多的问题，如跨域部署、依赖管理、压缩、按需加载、请求合并等。
-
-如何才是最佳的前端模板组织方案呢？
-
-##	关于 atc
-
-atc 即 artTemplate compiler，基于 [artTemplate](https://github.com/aui/artTemplate) 实现的前端模板预编译器，使用它可以让前端模版不再受浏览器的限制，支持如后端模版一样按文件放置、include 语句等特性：模板使用者无需关心模板内部的依赖，子模板会自动提前载入。
-
-编译后的模板不再依赖模板引擎，模板可以通过 [SeaJS](http://seajs.org) 或 [RequireJS](http://requirejs.org) 等加载器进行异步加载，亦能利用它们成熟的打包合并工具进行上线前的优化，如合并与压缩。
-
-##	三分钟上手
-
-跑一个演示例子就能明白如何使用了，以批处理版本为例：
-
-首先 [下载 atc](https://github.com/cdc-im/atc/archive/master.zip) 并解压。
-
-双击 compiler.cmd 即可运行，打开后它会自动编译 ./demo/templates/ 中所有模板文件，并生成同名 js 文件。模板预编译一切就这么简单！
-
-编译结束后你可以打开 [./demo/index.html](http://cdc-im.github.io/atc/demo/)，看其模板如何被浏览器加载。
-
-其中演示模板 ./demo/templates/index.html 使用了``include``语句引用了多个公用模板，但使用者无需关心模板内部的包含关系，公用模板会被自动提前加载。
-
-##	版本
-
-###	Windows 批处理版本
-
-``compiler.cmd ``无依赖第三方环境，双击即可运行，简单实用。打开后它会自动编译 ./demo/templates/ 中所有模板文件（你可以右键编辑 compiler.cmd 的配置修改模板目录）
-
-###	NodeJS 版本
-
-####	特性
-
-*	基于 NodeJS 跨平台
-*	实时编译，监控模板修改（默认不开启）
-*	检测模板语法错误
-
-####	安装
-
-	npm install template-compiler -g
-
-####	调用
-
-	atc [options] path
-
-其中``path``是前端模板目录，例如：``atc -w demo/templates``
-
-####	[options] 配置参数
-            
-*	``-w``或``--watch``设置实时监控模板修改
-*	``-d``或``--define-syntax``设置使用简单模板语法编译模板（自动加载语法扩展）
-*	``-c charset``或``--charset charset``定义模板编码，默认``utf-8``
-*	``-o path``或``--output path``定义输出目录，默认输出到模板目录
-*	``--clone-helpers``设置辅助方法克隆到编译后的函数，默认外置($helpers.js)
-*	``--version``显示 atc 版本号
-*	``--help``显示帮助信息
-
-##	模板语法
-
-atc 是 [artTemplate](https://github.com/aui/artTemplate) 的子项目，语法也与其保持一致，亦可通过它的插件简化模板语法。
-
-###	原生语法
-
-默认支持原生语法，正如本文开头的模板示例便就是 atc 支持的标准语法。
-
-模板使用``<%``与``%>``作为逻辑语句开始与闭合的标签，输出变量使用``=``或者``==``号开头，不同的是前者会对内容进行编码，以避免 XSS 漏洞。
-
-###	简版语法
-
-如果想使用简单的模板语法，可以把``./lab/template-syntax.js``合并至``./lab/template.js``即可（NodeJS版本开启``-d``参数会自动加载语法扩展，无需手动合并）。
-
-本文开头的模板示例使用简单语法可以简化为：
-
-	{include './public/header'}
-	<h3>{title}</h3>
-	<ul>
-	    {each list}
-	       <li>
-	        	<a href="{$value.url}">
-	        		{$value.title}
-	        	</a>
-	       </li>
-	    {/each}
-	</ul>
-	{include './public/footer'}
-	
-[简单语法详情](http://aui.github.com/artTemplate/extensions/index.html)
-
-##	模板 include 语句规范
-
-为了让编译工具能够进行静态分析，需要如下约定：
-
-1.	路径不能带后缀名
-2.	路径不能够进行字符串运算
-3.	路径不能使用变量
-
-**以下三种写法都是错误的：**
-
-1.	``<%include('./index.html')%>``路径不能带后缀名
-2.	``<%include('.' + '/idnex')%>``路径不能够进行字符串运算
-3.	``<%include(value)%>``路径不能使用变量
-
-##	更新日志
-
-###	v1.0.2
-
-NodeJS版本：
-
-*	增加``-o path``或``--output path``定义输出目录
-*	修复``-d``或``--define-syntax``可能失效的问题
-*	修改``-w``或``--watch``参数启动后的规则：只监控模板修改，而不再编译所有模板
-*	增强调试特性：模板语法错误将在控制台输出调试源码，并停止进程
-
-###	v1.0.1
-
-NodeJS版本：
-
-*	支持监控目录，即时编译
-*	使用命令行传递参数
-*	使用 npm 管理包
-*	支持设置简洁语法
-
-###	v1.0.0
-
-*	发布支持 NodeJS 与 Windows 批处理版本
-
-
-##	atc 用户
-
-正在完善中.. [atc 用户+1](https://github.com/cdc-im/atc/issues/1)
-
-© cdc.tencent.com
+    ;
+    (function () {
+        var helpers = namespace('taobao.artTempHelper');
+        var Render = function ($data) {
+                'use strict';
+                var $helpers = this,
+                    $out = '';
+                $out += '<div id="header"> <h1 id="logo"><a href="http://www.qq.com"><img width="134" height="44" src="http://mat1.gtimg.com/www/images/qq2012/qqlogo_1x.png" alt="腾讯网" /></a></h1> <ul id="nav"> <li><a href="http://www.qq.com">首页</a></li> <li><a href="http://news.qq.com/">新闻</a></li> <li><a href="http://pp.qq.com/">图片</a></li> <li><a href="http://mil.qq.com/">军事</a></li> </ul> </div>';
+                return new String($out)
+            };
+        Render.prototype = helpers;
+        var result = function (data) {
+                return new Render(data) + '';
+            }
+        namespace('taobao.header', result);
+    })();
--- compiler.js
+++ compiler.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 /*!
-:: -----------------------------------------------------------------------------
-:: atc - Template Compiler
-:: https://github.com/cdc-im/atc
-:: Released under the MIT, BSD, and GPL Licenses
-:: -----------------------------------------------------------------------------
-*/
+ :: -----------------------------------------------------------------------------
+ :: atc - Template Compiler
+ :: https://github.com/cdc-im/atc
+ :: Released under the MIT, BSD, and GPL Licenses
+ :: -----------------------------------------------------------------------------
+ */
 
 'use strict';
 
@@ -26,7 +26,8 @@ var compiler = {
         charset: 'utf-8',
         watch: false,
         cloneHelpers: false,
-        defineSyntax: false
+        defineSyntax: false,
+        namespace: null
     },
 
 
@@ -56,6 +57,9 @@ var compiler = {
             '    --version',
             '    [grey]display the version of atc[/grey]',
 
+            '    --namespace',
+            '    [grey]using namespace not seajs/require[/grey]',
+
             '    --help',
             '    [grey]show this help infomation[/grey]'
         ].join('\n') + '\n\n');
@@ -73,6 +77,7 @@ var compiler = {
     // 公用的辅助模块名字
     HELPERSNAME: '$helpers.js',
 
+    HELPERNAMESAPCE : 'artTempHelper',
     /*
      * 模板编译引擎
      * @param   {String}    模板
@@ -83,7 +88,6 @@ var compiler = {
 
         template.isCompress = true;
 
-
         // 提取include模板
         var REQUIRE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^/\r\n])+\/(?=[^\/])|\/\/.*|\.\s*include|(?:^|[^$])\binclude\s*\(\s*(["'])(.+?)\1\s*(,\s*(.+?)\s*)?\)/g; //"
         var SLASH_RE = /\\\\/g
@@ -93,52 +97,68 @@ var compiler = {
             var uniq = {};
 
             code
-            .replace(SLASH_RE, "")
-            .replace(REQUIRE_RE, function(m, m1, m2) {
-                if (m2 && !uniq.hasOwnProperty(m2)) {
-                    ret.push(m2);
-                    uniq[m2] = true;
-                }
-            });
+                .replace(SLASH_RE, "")
+                .replace(REQUIRE_RE, function (m, m1, m2) {
+                    if (m2 && !uniq.hasOwnProperty(m2)) {
+                        ret.push(m2);
+                        uniq[m2] = true;
+                    }
+                });
 
-          return ret
+            return ret
         };
 
 
         // 包装成RequireJS、SeaJS模块
-        var toModule = function (code, helpersPath) {
+        var toModule = function (code, helpersPath, that) {
+            //TODO STEP3
+            var pkg = that.options['namespace'],
+                useNamespace = !!pkg,
+                namespace = pkg + '.' + (function(file){
+                    var splits = file.split(/[/\.]/);
+                    return  splits[splits.length - 2];
+                })(that.file);
 
             template.onerror = function (e) {
                 throw e;
             };
 
-
             // 使用artTemplate编译模板
             var render = template.compile(code);
             var prototype = render.prototype;
-            
 
             render = render.toString()
-            .replace(/^function\s+(anonymous)/, 'function');
-
+                .replace(/^function\s+(anonymous)/, 'function');
 
             // SeaJS与RequireJS规范，相对路径前面需要带“.”
+
             var fixPath = function (dir) {
+                if(!useNamespace){
                 dir = dir
-                .replace(/\\/g, '/')
-                .replace(/\.js$/, '');
+                    .replace(/\\/g, '/')
+                    .replace(/\.js$/, '');
 
                 if (!/^(\.)*?\//.test(dir)) {
                     dir = './' + dir;
                 }
+                }
                 return dir;
             };
 
+            //TODO STEP4  requires
+            var require = function(file){
+                if(useNamespace){
+                    return 'namespace(\'' + file  + '\')';
+                }else{
+                    return  'require(\'' + fixPath(file) + '\')';
+                }
+            };
 
+            //TODO STEP5  dependencys
             var dependencies = [];
             parseDependencies(render).forEach(function (file) {
                 dependencies.push(
-                    '\'' + file + '\': ' + 'require(\'' + fixPath(file) + '\')'
+                    '\'' + fixPath(file) + '\': '  + require(file)
                 );
             });
             var isDependencies = dependencies.length;
@@ -148,16 +168,11 @@ var compiler = {
             // 输出辅助方法
             var helpers;
 
+            //TODO STEP6 helpers
             if (helpersPath) {
-
-                helpersPath = fixPath(helpersPath);
-
-                helpers = 'require(\'' + helpersPath + '\')';
-
+                helpers =  require( pkg + '.' + that.TEMPLATE_NAME);
             } else {
-
                 helpers = [];
-                
                 for (var name in prototype) {
 
                     if (name !== '$render') {
@@ -169,42 +184,58 @@ var compiler = {
                 helpers = '{' + helpers.join(',') + '}';
             }
 
+            if(that){
+                code =
+                    ';(function() {'
+                        + (isDependencies ? 'var dependencies=' + dependencies + ';' : '')
+                        + 'var helpers = ' + helpers + ';'
+                        + (isDependencies ? 'var $render=function(id,data){'
+                        + 'return dependencies[id](data);'
+                        + '};' : '')
+                        + 'var Render=' + render + ';'
+                        + 'Render.prototype=helpers;'
+                        + 'var result = function(data){'
+                        + (isDependencies ? 'helpers.$render=$render;' : '')
+                        + 'return new Render(data) + \'\';'
+                        + '}'
+                        + 'namespace(\''  + namespace + '\',result);'
+                        + '})();';
+            }else{
+                code =
+                'define(function(require) {'
+                    + (isDependencies ? 'var dependencies=' + dependencies + ';' : '')
+                    + 'var helpers = ' + helpers + ';'
+                    + (isDependencies ? 'var $render=function(id,data){'
+                    + 'return dependencies[id](data);'
+                    + '};' : '')
+                    + 'var Render=' + render + ';'
+                    + 'Render.prototype=helpers;'
+                    + 'return function(data){'
+                    + (isDependencies ? 'helpers.$render=$render;' : '')
+                    + 'return new Render(data) + \'\';'
+                    + '}'
+                    + '});';
+            }
 
-            code =
-            'define(function(require) {'
-            +   (isDependencies ? 'var dependencies=' + dependencies + ';' : '')
-            +   'var helpers = ' + helpers + ';'
-            +   (isDependencies ? 'var $render=function(id,data){'
-            +       'return dependencies[id](data);'
-            +   '};' : '')
-            +   'var Render=' + render  + ';'
-            +   'Render.prototype=helpers;'
-            +   'return function(data){'
-            +       (isDependencies ? 'helpers.$render=$render;' : '')
-            +       'return new Render(data) + \'\';'
-            +   '}'
-            + '});';
-            
-            
             return code;
         };
 
-        return function (source, helpersPath) {
-            return toModule(source, helpersPath);
+        return function (source, helpersPath, that) {
+            return toModule(source, helpersPath, that);
         }
 
     })(),
 
-    
+
     /** 格式化JS */
-    format: function(code) {
+    format: function (code) {
 
         if (typeof js_beautify !== 'undefined') {
 
             js_beautify =
-            typeof js_beautify === 'function'
-            ? js_beautify
-            : js_beautify.js_beautify;
+                typeof js_beautify === 'function'
+                    ? js_beautify
+                    : js_beautify.js_beautify;
 
             var config = {
                 indent_size: 4,
@@ -226,7 +257,7 @@ var compiler = {
     writeHelpers: function () {
 
         var helpers = [];
-        var fullname = this._path  + '/' + this.HELPERSNAME;
+        var fullname = this._path + '/' + this.HELPERSNAME;
         var prototype = template.prototype;
 
         for (var name in prototype) {
@@ -237,12 +268,20 @@ var compiler = {
 
         helpers = '{\n' + helpers.join(',\n') + '}';
 
-        var module = 'define(function () {'
-        +    'return ' + helpers
-        + '});'
+        var module = '';
+        //FIXME
+        if(this.options['namespace']){
+            module = ';(function () { '
+                + 'namespace(\'' + this.options['namespace'] + '.' +  this.HELPERNAMESAPCE + '\','
+                + helpers
+                + ')})();'
+        }else{
+            module = 'define(function () {'
+                + 'return ' + helpers
+                + '});'
+        }
 
         module = this.format(module);
-
         this._fsWrite(fullname, module);
     },
 
@@ -254,20 +293,20 @@ var compiler = {
     log: function (message) {
         var styles = {
             // styles
-            'bold'      : ['\x1B[1m',  '\x1B[22m'],
-            'italic'    : ['\x1B[3m',  '\x1B[23m'],
-            'underline' : ['\x1B[4m',  '\x1B[24m'],
-            'inverse'   : ['\x1B[7m',  '\x1B[27m'],
+            'bold': ['\x1B[1m', '\x1B[22m'],
+            'italic': ['\x1B[3m', '\x1B[23m'],
+            'underline': ['\x1B[4m', '\x1B[24m'],
+            'inverse': ['\x1B[7m', '\x1B[27m'],
             // colors
-            'white'     : ['\x1B[37m', '\x1B[39m'],
-            'grey'      : ['\x1B[90m', '\x1B[39m'],
-            'black'     : ['\x1B[30m', '\x1B[39m'],
-            'blue'      : ['\x1B[34m', '\x1B[39m'],
-            'cyan'      : ['\x1B[36m', '\x1B[39m'],
-            'green'     : ['\x1B[32m', '\x1B[39m'],
-            'magenta'   : ['\x1B[35m', '\x1B[39m'],
-            'red'       : ['\x1B[31m', '\x1B[39m'],
-            'yellow'    : ['\x1B[33m', '\x1B[39m']
+            'white': ['\x1B[37m', '\x1B[39m'],
+            'grey': ['\x1B[90m', '\x1B[39m'],
+            'black': ['\x1B[30m', '\x1B[39m'],
+            'blue': ['\x1B[34m', '\x1B[39m'],
+            'cyan': ['\x1B[36m', '\x1B[39m'],
+            'green': ['\x1B[32m', '\x1B[39m'],
+            'magenta': ['\x1B[35m', '\x1B[39m'],
+            'red': ['\x1B[31m', '\x1B[39m'],
+            'yellow': ['\x1B[33m', '\x1B[39m']
         };
 
         styles['b'] = styles['bold'];
@@ -276,8 +315,8 @@ var compiler = {
 
         message = message.replace(/\[([^\]]*?)\]/igm, function ($1, $2) {
             return $2.indexOf('/') === 0
-            ? styles[$2.slice(1)][1]
-            : styles[$2][0];
+                ? styles[$2.slice(1)][1]
+                : styles[$2][0];
         });
 
         process.stdout.write(message);
@@ -291,10 +330,10 @@ var compiler = {
         var watchList = {};
         var timer = {};
 
-        function walk (dir) {
+        function walk(dir) {
             fs.readdirSync(dir).forEach(function (item) {
                 var fullname = dir + '/' + item;
-                if (fs.statSync(fullname).isDirectory()){
+                if (fs.statSync(fullname).isDirectory()) {
                     watch(fullname);
                     walk(fullname);
                 }
@@ -302,15 +341,15 @@ var compiler = {
         };
 
         // 排除“.”、“_”开头或者非英文命名的目录
-        function filter (name) {
+        function filter(name) {
             return !that.FILTER_RE.test(name);
         };
 
-        function watch (parent) {
+        function watch(parent) {
 
             var target = path.basename(parent);
 
-            if (!filter(target)){
+            if (!filter(target)) {
                 return;
             }
 
@@ -350,7 +389,7 @@ var compiler = {
                         fstype = 'file';
                         type = event == 'rename' ? 'create' : 'updated'
 
-                    // 文件夹
+                        // 文件夹
                     } else if (event === 'rename') {
 
                         fstype = 'directory';
@@ -368,11 +407,11 @@ var compiler = {
                     fstype: fstype
                 };
 
-                
+
                 if (/windows/i.test(require('os').type())) {
                     // window 下 nodejs fs.watch 不稳定(nodejs v0.10.5)
                     clearTimeout(timer[fullname]);
-                    timer[fullname] = setTimeout(function() {
+                    timer[fullname] = setTimeout(function () {
                         callback(eventData);
                     }, 16);
 
@@ -439,7 +478,7 @@ var compiler = {
 
     // 删除模板文件
     _fsUnlink: function (file) {
-        file = file.replace(this._path , this._output);
+        file = file.replace(this._path, this._output);
         return fs.existsSync(file) && fs.unlink(file);
     },
 
@@ -450,7 +489,7 @@ var compiler = {
         var that = this;
 
         // 监控模板目录
-        this._onwatch(this._path , function (event) {
+        this._onwatch(this._path, function (event) {
             var type = event.type;
             var fstype = event.fstype;
             var target = event.target;
@@ -463,8 +502,7 @@ var compiler = {
                 if (type === 'delete') {
                     var js = fullname.replace(that.EXTNAME_RE, '.js');
                     that._fsUnlink(js);
-                } else
-                if (type === 'updated' || type === 'create') {
+                } else if (type === 'updated' || type === 'create') {
                     that.compile(fullname);
                 }
             }
@@ -480,6 +518,8 @@ var compiler = {
      * @return  {Boolean}   成功true, 失败false
      */
     compile: function (file) {
+        //FIXME
+        console.log(file);
 
         var name = null;
         var that = this;
@@ -489,15 +529,15 @@ var compiler = {
         if (!this.options['cloneHelpers']) {
             name = this.HELPERSNAME;
             var dirname = path.dirname(file);
-            var join = path.join(this._path , name);
+            var join = path.join(this._path, name);
             name = path.relative(dirname, join);
         }
 
 
         var source = this._fsRead(file);
         var target = file
-        .replace(this.EXTNAME_RE, '.js')
-        .replace(this._path , this._output);
+            .replace(this.EXTNAME_RE, '.js')
+            .replace(this._path, this._output);
 
         var info = file.replace(this._path, '');
         var info_source = this.options['path'] + info;
@@ -506,8 +546,15 @@ var compiler = {
         this.log('Compile: [green]' + info_source + '[/green]');
 
         try {
-            
-            var code = this.engine(source, name);
+
+            //TODO STEP2!!!
+            var code = this.engine(source, name, {
+                options : this.options,
+                file : info_output,
+                TEMPLATE_NAME : this.HELPERNAMESAPCE
+
+            });
+
             code = this.format(code);
             this._fsWrite(target, code);
             this.log('[grey] > ' + info_output + '[/grey]\n');
@@ -526,10 +573,10 @@ var compiler = {
 
     // 调试语法错误
     _debug: function (code) {
-        
+
         var code = this.format(code);
         var debugFile = this._output + '/.debug.js';
-        
+
         try {
             this._fsWrite(debugFile, code);
             require(debugFile);
@@ -559,7 +606,7 @@ var compiler = {
 
         };
 
-        walk(this._path );
+        walk(this._path);
     },
 
 
@@ -604,6 +651,12 @@ var compiler = {
                     options.charset = args.shift().toLowerCase();
                     break;
 
+                ////TODO STEP1!!!
+                case '--namespace':
+                    options.namespace = args.shift().toLowerCase();
+//                    options.cloneHelpers = true;
+                    break;
+
                 // 版本号
                 case '--version':
                     return this.log(this.version + '\n');
@@ -635,7 +688,8 @@ var compiler = {
         if (!fs.existsSync(options['path'])) {
             console.log('Error: directory does not exist');
             return process.exit(1);
-        };
+        }
+        ;
 
 
         options['path'] = options['path'].replace(/[\/\\]$/, '');
